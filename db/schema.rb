acf3c206afcc63787c652b904a693f62016079c3
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2021-10-05 22:59:28 +0900

    fontAwesome

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_111651) do
+ActiveRecord::Schema.define(version: 2021_10_05_133854) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2021_09_30_111651) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.string "image"
+    t.boolean "solved"
   end
 
   create_table "users", force: :cascade do |t|
