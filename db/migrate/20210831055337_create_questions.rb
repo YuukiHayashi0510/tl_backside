88e67ca5d6c086ddd8257f772348727b9c50cbe4
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-02-27 20:03:34 +0900

    fix:Ruby最新版に適応＆Macに適応

--- db/migrate/20210831055337_create_questions.rb
+++ db/migrate/20210831055337_create_questions.rb
@@ -1,7 +1,6 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-      t.string :title
       t.text :body
       t.integer :user_id
       t.string :image
