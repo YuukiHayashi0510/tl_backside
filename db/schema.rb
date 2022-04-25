2889d0773ed030e60c15112e0fe12f916c2e9203
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-04-25 18:13:31 +0900

    macVersion bundle.lock

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,112 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_09_24_155930) do
 
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.integer "lecture_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lecture_id"], name: "index_comments_on_lecture_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "current_lectures", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "lecture_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lecture_id"], name: "index_current_lectures_on_lecture_id"
+    t.index ["user_id"], name: "index_current_lectures_on_user_id"
+  end
+
+  create_table "evaluations", force: :cascade do |t|
+    t.integer "overall"
+    t.integer "understandability"
+    t.integer "usefulness"
+    t.integer "easy"
+    t.integer "assignment"
+    t.integer "user_id"
+    t.integer "lecture_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "lectures", force: :cascade do |t|
+    t.string "name"
+    t.string "teacher"
+    t.integer "grade"
+    t.string "term"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "question_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_likes_on_question_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "past_lectures", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "lecture_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lecture_id"], name: "index_past_lectures_on_lecture_id"
+    t.index ["user_id"], name: "index_past_lectures_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.string "image"
+    t.boolean "solved", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.text "profile"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
+  add_foreign_key "comments", "lectures"
+  add_foreign_key "comments", "users"
+  add_foreign_key "current_lectures", "lectures"
+  add_foreign_key "current_lectures", "users"
+  add_foreign_key "likes", "questions"
+  add_foreign_key "likes", "users"
+  add_foreign_key "past_lectures", "lectures"
+  add_foreign_key "past_lectures", "users"
 end
