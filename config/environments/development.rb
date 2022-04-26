aaa9621c2a075eecdedb493d8148ce61d590827e
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-04-26 16:44:52 +0900

    envに追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,16 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: ENV['SEND_MAIL'],
+    password: ENV['GMAIL_SPECIFIC_PASSWORD'],
+    authentication: :login,
+    openssl_verify_mode: 'none',
+    enable_starttls_auto: true
+  }
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
 
